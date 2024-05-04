47171db5ee83504b3287693f9b46f0d001e52ce1
Seasawher <tellagoodstory@icloud.com>
2024-05-04 14:48:04 +0900

    use lake script to test

--- lakefile.lean
+++ lakefile.lean
@@ -10,3 +10,19 @@ lean_lib «MkExercise» where
 @[default_target]
 lean_exe «mk_exercise» where
   root := `MkExercise
+
+def runCmd (cmd : String) (args : Array String) : ScriptM Bool := do
+  let out ← IO.Process.output {
+    cmd := cmd
+    args := args
+  }
+  let hasError := out.exitCode != 0
+  if hasError then
+    IO.eprint out.stderr
+  return hasError
+
+/-- run test by `lake test` -/
+@[test_runner] script test do
+  if ← runCmd "lake" #["exe", "mk_exercise", "Test/Src", "Test/Out"] then return 1
+  if ← runCmd "lean" #["--run", "Test.lean"] then return 1
+  return 0
